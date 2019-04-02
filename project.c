572680d37341f4ce7c63a6f3974134074b35e481
Joel Mackenzie <joel.mackenzie@uon.edu.au>
2019-04-02 23:48:12 +0000

    ftyguh

--- project.c
+++ project.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
 int main() {
-return 0;
+    // Zeros first N elements of x
+    void zero(int *x, int N) {
+        int i=1; // Array index loop counter
+        for(i = 0; i < N; i++)
+        x[i] = 0; // Use array syntax
+        return; // Optional
+        }
+
+    return 0;
 }
